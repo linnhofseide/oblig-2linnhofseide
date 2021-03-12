cc4336dc2f149b4c7441887e0cdd79032ff84ea0
Linn Hofseide <linn.hofseide@gmail.com>
2021-03-12 17:15:27 +0100

    oppdaterer filene

---
+++ src/main/java/com/example/demo/KinoOrdre.java
@@ -0,0 +1,68 @@
+package com.example.dehdudhudhmo;
+
+public class KinoOrdre {
+    private String filmer;
+    private String antall;
+    private String fornavn;
+    private String etternavn;
+    private String tlf;
+    private String epost;
+
+    public KinoOrdre(String filmer, String antall, String fornavn, String etternavn, String tlf, String epost){
+        this.filmer = filmer;
+        this.antall = antall;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.tlf = tlf;
+        this.epost = epost;
+    }
+
+    public String getFilmer(){
+        return filmer;
+    }
+
+    public void setFilmer(String filmer) {
+        this.filmer = filmer;
+    }
+
+    public String getAntall(){
+        return antall;
+    }
+
+    public void setAntall(String antall){
+        this.antall = antall;
+    }
+
+    public String getFornavn(){
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn){
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn){
+        this.etternavn = etternavn;
+    }
+
+    public String getTlf(){
+        return tlf;
+    }
+
+    public void setTlf(String tlf){
+        this.tlf = tlf;
+    }
+
+    public String getEpost(){
+        return epost;
+    }
+
+    public void setEpost(String epost){
+        this.epost = epost;
+    }
+
+}
